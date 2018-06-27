7a6e2b4706db22de9f0a144d1e150ede9ae92400
hannahgreen1 <hannahgreen94@icloud.com>
2018-06-27 17:13:43 +0100

    all done

--- src/app.js
+++ src/app.js
@@ -1,3 +1,17 @@
+const Continents = require('./models/continents.js');
+const ContinentListView = require('./views/country_list_view.js');
+const CountriesInfoView = require('./views/country_info.js');
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript Loaded');
+  const continentsListContainer = document.querySelector('#countries');
+  const countryData = document.querySelector('#country');
+
+  const continentsListView = new ContinentListView(continentsListContainer);
+  continentsListView.bindEvents();
+
+  const countryInfo = new CountriesInfoView(countryData);
+  countryInfo.bindEvents();
+
+  const continents = new Continents();
+  continents.getData();
 });
